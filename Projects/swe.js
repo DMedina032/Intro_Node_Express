b7d9df9d6ddf6887ec44791c9b26fedfda74f612
dajah <dmedina032@west-mec.org>
2019-10-10 18:19:56 -0700

    linked in ur pages into pug
    need to link in our API's

--- Projects/swe.js
+++ Projects/swe.js
@@ -1,13 +1,14 @@
 var express = require('express');
 var app = express();
 var PORT = 3000;
+const path = require('path');
 
 app.get('../', function(req, res){
   res.send('Hello world');
 });
 
 app.set("view engine", "pug")
-app.set("views" , path.res("../"));
+app.set("views" , 'views');
 // app.get('/', function(req, res){
 //     res.sendFile(__dirname + '/index.pug');
 // });
@@ -25,21 +26,25 @@ var server = app.listen(3000, function(){
     var port = server.address().port;
     console.log('app listening at http://localhost:3000', host, port);
 });
+
+
+
+ 
 // console.log("app.listen() executed.");
 
 // ...............
 // 
-app.set("view engine", "pug");
-app.set("views", path.join(__dirname, "views"));
-app.get("/", (request, response) =>{
-  response.render('index.pug');
-});
-app.get("/about", (request, response) =>{
-  response.render('index.pug',{
-    title:'thinking critically ',
-    message:'researching node and express'}
-    );
-});
+// app.set("view engine", "pug");
+// app.set("views", path.join(__dirname, "views"));
+// app.get("/", (request, response) =>{
+//   response.render('index.pug');
+// });
+// app.get("/about", (request, response) =>{
+//   response.render('index.pug',{
+//     title:'thinking critically ',
+//     message:'researching node and express'}
+//     );
+// });
 // app.use(function(req, res, next){
 //   if(err){
 //     res.status(err.status || 500)
@@ -47,16 +52,26 @@ app.get("/about", (request, response) =>{
 //     .send(err.message || 'SERVER ERROR');
 //   }
 // })
-let listener = app.listen(process.env.PORT || PORT, () =>{
-  console.log('' + listener.address().port);
-});
+// let listener = app.listen(process.env.PORT || PORT, () =>{
+//   console.log('' + listener.address().port);
+// });
 
 
 
 
 
-router.get("/", (req, res, next) => {
-let url = "localhost:3000"
-res.render("index", {url:url});
-});
+
+app.get("/", (req, res, next) => {
+  let url = "localhost:3000"
+  res.render("index", {url:url});
+  });
+  
+
+app.get("/contact", (req, res, next) => {
+  let url = "localhost:3000"
+  res.render("contact", {url:url});
+  });
+  
+  
+  
 
